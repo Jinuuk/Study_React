aaed6a13de9477e61ff1993e6e304611f281ecb7
Jinuk Kim <zpd29thd03@naver.com>
2022-10-22 15:50:22 +0900

    2022.10.22(2)

--- study_react/src/App.js
+++ study_react/src/App.js
@@ -8,7 +8,13 @@ function App() {
 
   let post = '울산 라면 맛집';
   let [title, titleChange] = useState(['남자 코트 추천', '남자 바지 추천', '남자 신발 추천']); //디스트럭처링
-  let [like, likeChange] = useState(0);
+  let [like, likeChange] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
+
+  // [1,2,3].map(function(ele){
+  //   console.log(ele)
+  // })
+
 
   // function like_fnc(){
   //   console.log(1);
@@ -31,7 +37,7 @@ function App() {
           titleChange(titleCopy2.sort());
         }}>가나다순 정렬</button>
 
-      <div className="list"> 
+      {/* <div className="list"> 
         <h4>{ title[0] } <span onClick={ () => { likeChange(like+1) } }>👍</span> { like } </h4>
         <p>2월 17일 발행</p>
       </div>
@@ -40,11 +46,53 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ title[2] }</h4>
+        <h4 onClick={()=>{
+            modal==true ? setModal(false) : setModal(true)
+           }}>{ title[2] }</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {
+        modal==true? <Modal/> : null
+      }
+
+      {/* <Modal/> */}
+
+      {
+        title.map(function(ele, i){
+          return (
+            <div className="list" key={i}>
+          <h4>{ ele } <span onClick={ () => {
+            let likeCopy = [...like];
+            likeCopy[i]=likeCopy[i]+1;
+              likeChange(likeCopy);
+            } }>👍</span> { like[i] }</h4>
+          {/* <h4>{ title[i] }</h4> */}
+          <p>2월 17일 발행</p>
+        </div>
+        )
+        })
+      }
+
     </div>
   );
 }
+
+function Modal(){
+  return (
+      <div className="modal" >
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
+}
+
+// let Modal = () => {
+//   return (
+//     <div></div>
+//   )
+// }
+
 
 export default App;
