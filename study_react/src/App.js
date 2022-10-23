3108ee60c0c40ea40edfd25fa2e7416313564927
Jinuk Kim <zpd29thd03@naver.com>
2022-10-23 13:47:49 +0900

    2022.10.23

--- study_react/src/App.js
+++ study_react/src/App.js
@@ -7,9 +7,11 @@ import { useState } from 'react';
 function App() {
 
   let post = '울산 라면 맛집';
-  let [title, titleChange] = useState(['남자 코트 추천', '남자 바지 추천', '남자 신발 추천']); //디스트럭처링
-  let [like, likeChange] = useState([0,0,0]);
+  let [title, setTitle] = useState(['남자 코트 추천', '남자 바지 추천', '남자 신발 추천']); //디스트럭처링
+  let [modalTitle,setModalTitle] = useState(0);
+  let [like, setLike] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [inputText,setInputText] = useState(' ');
 
   // [1,2,3].map(function(ele){
   //   console.log(ele)
@@ -26,19 +28,19 @@ function App() {
         <h4>블로그</h4>
       </div>
 
-      <button onClick = { ()=>{
+      {/* <button onClick = { ()=>{
         let titleCopy = [...title];
         titleCopy[0] = '여자 코트 추천';
-        titleChange(titleCopy);
+        setTitle(titleCopy);
         } }>글수정</button>
 
         <button onClick={()=>{
           let titleCopy2 = [...title];
-          titleChange(titleCopy2.sort());
-        }}>가나다순 정렬</button>
+          setTitle(titleCopy2.sort());
+        }}>가나다순 정렬</button> */}
 
       {/* <div className="list"> 
-        <h4>{ title[0] } <span onClick={ () => { likeChange(like+1) } }>👍</span> { like } </h4>
+        <h4>{ title[0] } <span onClick={ () => { setLike(like+1) } }>👍</span> { like } </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
@@ -52,9 +54,6 @@ function App() {
         <p>2월 17일 발행</p>
       </div> */}
 
-      {
-        modal==true? <Modal/> : null
-      }
 
       {/* <Modal/> */}
 
@@ -62,28 +61,79 @@ function App() {
         title.map(function(ele, i){
           return (
             <div className="list" key={i}>
-          <h4>{ ele } <span onClick={ () => {
-            let likeCopy = [...like];
-            likeCopy[i]=likeCopy[i]+1;
-              likeChange(likeCopy);
-            } }>👍</span> { like[i] }</h4>
-          {/* <h4>{ title[i] }</h4> */}
-          <p>2월 17일 발행</p>
+              <h4 onClick={()=>{
+                modal==true ? setModal(false) : setModal(true);
+                setModalTitle(i);
+              }}>{ ele } <span onClick={ (e) => {
+                e.stopPropagation(); //이벤트 버블링 방지
+                let likeCopy = [...like];
+                likeCopy[i]=likeCopy[i]+1;
+                setLike(likeCopy);
+              } }>👍</span> { like[i] }</h4>
+              {/* <h4>{ title[i] }</h4> */}
+              
+              <p>2월 17일 발행</p>
+              <button onClick={()=>{
+                let titleCopy = [...title];
+                titleCopy.splice(i,1);
+                setTitle(titleCopy);
+              }}>삭제</button>
         </div>
         )
-        })
-      }
+      })
+    }
+    
+    <input onChange={e=>{
+      setInputText(e.target.value);  //늦게 처리됨 (비동기 처리)
+      console.log(inputText);}} />
+    <button onClick={()=>{
+      let titleCopy = [...title];
+      titleCopy.push(inputText);
+      setTitle(titleCopy);
+      like.push(0);
+    }}>등록</button>
 
+    {/* {
+      modal==true? 
+        <Modal color="skyblue"  title = {title[modalTitle]} modifyFnc = { ()=>{
+          let titleCopy = [...title];
+          titleCopy[0] = '여자 코트 추천';
+          setTitle(titleCopy);
+          } }/> : null
+    } */}
+
+    {
+      modal==true? 
+        <Modal color="skyblue"  title = {title} modalTitle ={modalTitle} modifyFnc = { ()=>{
+          let titleCopy = [...title];
+          titleCopy[0] = '여자 코트 추천';
+          setTitle(titleCopy);
+          } }/> : null
+    }
+
+    
     </div>
   );
 }
+ 
+// function Modal(props){
+//   return (
+//       <div className="modal"  style={{background : props.color}}>
+//         <h4>{props.title}</h4>
+//         <p>날짜</p>
+//         <p>상세내용</p>
+//         <button onClick = {props.modifyFnc}>글수정</button>
+//       </div>
+//   )
+// }
 
-function Modal(){
+function Modal(props){
   return (
-      <div className="modal" >
-        <h4>제목</h4>
+      <div className="modal"  style={{background : props.color}}>
+        <h4>{props.title[props.modalTitle]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick = {props.modifyFnc}>글수정</button>
       </div>
   )
 }
