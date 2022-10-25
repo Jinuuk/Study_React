99031dd5ff19cdc6eef9251dd66be568d1d4d4aa
Jinuk Kim <zpd29thd03@naver.com>
2022-10-25 10:59:25 +0900

    2022.10.25

--- study_react2/src/App.js
+++ study_react2/src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 // import 작명 from './data.js';
 // import {a,b} from './data.js';
 import data from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
+import Detail from './routes/Detail.js';
 
 
 function App() {
 
   let [shoes] = useState(data)
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -17,41 +20,51 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">KimShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            {/* <Link to="/" className="menu">Home</Link>
+            <Link to="/detail" className="menu">Detail</Link> */}
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            {/* <Nav.Link onClick={()=>{navigate(-1)}}>Home</Nav.Link> */}
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/about')}}>About</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/event')}}>Event</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-    {/* <div className="main-bg" style={{backgroundImage : 'url('+breadImage+')'}}></div> */}
-    <div className = "main-bg"></div>
-    <Container>
-      <Row>
-        {/* <Col sm>
-          <img src={process.env.PUBLIC_URL + '/bread2.jpg'} width="100%" />
-          <h4>{shoes[0].title}</h4>
-          <p>{shoes[0].price}</p>
-        </Col>
-        <Col sm>
-        <img src={process.env.PUBLIC_URL + '/bread3.jpg'} width="100%" />
-        <h4>{shoes[1].title}</h4>
-          <p>{shoes[1].price}</p>
-        </Col>
-        <Col sm>
-        <img src={process.env.PUBLIC_URL + '/bread4.jpg'}  width="100%" />
-        <h4>{shoes[2].title}</h4>
-          <p>{shoes[2].price}</p>
-        </Col> */}
-      {
-        shoes.map(function(ele,i){
-          return (
-            <Item item={ele} order={i+2}/>
-          )
-        })
-      }
-      </Row>
-    </Container>
+    <Routes>
+      <Route path="/" element={
+        <>
+          <div className = "main-bg"></div>
+          <Container>
+            <Row>
+            {
+              shoes.map(function(ele){
+                return (
+                  <Item item={ele}/>
+                )
+              })
+            }
+            </Row>
+          </Container>
+        </>
+      }/>
+      <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
+
+      <Route path="/about" element={<About/>}>
+        <Route path="member" element={<div>멤버</div>}/>
+        <Route path="location" element={<div>위치</div>}/>
+      </Route>
+
+      <Route path="/event" element={<Event/>}>
+        <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}/>
+        <Route path="two" element={<div>생일기념 쿠폰 받기</div>}/>
+      </Route>
+
+
+
+      <Route path="*" element={<div>404 없는 페이지</div>}/>
+    </Routes>
+
     </div>
   );
 }
@@ -59,11 +72,32 @@ function App() {
 function Item(props) {
   return(
     <Col sm>
-    <img src={process.env.PUBLIC_URL + '/bread'+props.order+'.jpg'} width="100%" />
+    <img src={process.env.PUBLIC_URL + '/bread'+(props.item.id+2)+'.jpg'} width="100%" />
     <h4>{props.item.title}</h4>
     <p>{props.item.price}</p>
   </Col>
   )
 }
+
+function About(){
+  return (
+    <div>
+      <h4>about 페이지</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event(){
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
+
 
 export default App;
